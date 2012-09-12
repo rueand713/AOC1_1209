56c957c32f7d26344c17b56cf0e71dcb7c7cce80
Rueben Anderson <rueand713@fullsail.edu>
2012-09-12 02:44:32 -0500

    Finalized Project 3

--- project3/project3/project3/ViewController.h
+++ project3/project3/project3/ViewController.h
@@ -9,5 +9,20 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    
+}
+
+// create the add (#1) and compare (#2) functions
+// the add takes two int types and returns an int while the compare
+// function accepts two nsinteger types and returns a BOOL type
+-(int)add:(int)val1 val2:(int)val2;
+-(BOOL)compare:(NSInteger)nsVal1 nsVal2:(NSInteger)nsVal2;
+
+// create the append (#3) and displayAlertWithString (#5) functions
+// the append function accepts two NSString types appends them and returns a new NSString
+// the displayAlertWithString function takes 1 NSString type and displays it as a UIAlertView
+-(NSString*)append:(NSString*)str1 str2:(NSString*)str2;
+-(void)dislplayAlertWithString:(NSString*)alertStr;
 
 @end
--- project3/project3/project3/ViewController.m
+++ project3/project3/project3/ViewController.m
@@ -16,6 +16,44 @@
 
 - (void)viewDidLoad
 {
+    // define initial variables
+    int int_a = 6;
+    int int_b = 6;
+    NSInteger nsi_a = int_a;
+    NSInteger nsi_b = int_b;
+    NSString *strText = [[NSString alloc] initWithString:@""];
+    
+    // check if strText object exists
+    if (strText != nil)
+    {
+        // call the append function and captures the result in the strText variable (#4)
+        strText = [self append:@"This is an alert. " str2:@"Just press the 'Accept' button."];
+        
+        // pass in the appended values to be displayed in a UIAlertView
+        [self dislplayAlertWithString:strText];
+        
+    }
+    
+    // call the add function and capture the result in the answer variable (#6)
+    // creates an NSNumber object ns_num that holds the value returned in answer (#7)
+    // creates an NSString object that will hold the converted ns_num value (#8)
+    int answer = [self add:(int) int_a val2: (int) int_b ];
+    NSNumber *ns_num = [[NSNumber alloc] initWithInt:answer];
+    NSString *numStr = [[NSString alloc] initWithFormat:@"The number is %@", ns_num ];
+    
+    // passes in the numStr object to be displayed as UIAlertView
+    [self dislplayAlertWithString:numStr];
+    
+    // call the compare function and capture the result in the comp variable (#9a)
+    BOOL comp = [self compare:nsi_a nsVal2:nsi_b];
+    
+    // if comp evaluates true display an UIAlertView of the values and YES (#9b)
+    if (comp) {
+        NSString *compText = [[NSString alloc] initWithFormat:@"%@ %d is equal to %d", comp ? @"YES" : @"NO", nsi_a, nsi_b ];
+        [self dislplayAlertWithString: compText];
+    }
+    
+        
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
@@ -29,6 +67,40 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+    // Defines the add function
+- (int)add:(int)val1 val2:(int)val2
+{
+    return val1 + val2;
+}
+
+    // Defines the compare function
+- (BOOL)compare:(NSInteger)nsVal1 nsVal2:(NSInteger)nsVal2
+{
+    BOOL equal = NO;
+    
+    if (nsVal1 == nsVal2){
+        equal = YES;
+    };
+    
+    return equal;
+}
+
+    // Defines the append function
+- (NSString*)append:(NSString *)str1 str2:(NSString *)str2
+{
+    NSMutableString *newString = [[NSMutableString alloc] initWithString:str1];
+    [newString appendString:str2];
+    
+    return newString;
+}
+
+    // Defines the displayAlertWithString function
+- (void)dislplayAlertWithString:(NSString*)alertStr
+{
+    UIAlertView *alertbox = [[UIAlertView alloc] initWithTitle:@"ALERT" message:alertStr delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+    [alertbox show];
 }
 
 @end
